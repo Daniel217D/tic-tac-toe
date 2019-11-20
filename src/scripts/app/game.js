c5f975f96d2f3aa78f63a13ce9c235b64cc56649
Daniil <daniel217032001@gmail.com>
2019-11-20 19:25:33 +0300

    add statuses

--- src/scripts/app/game.js
+++ src/scripts/app/game.js
@@ -1,8 +1,10 @@
 let field = Array(9);
 const squares = document.querySelectorAll(".js-square");
 const reset_btn = document.querySelector(".js-reset");
+const status_container = document.querySelector(".js-status");
 let game_is_active = true;
 let sign = "x";
+let turn = 0;
 
 const classes = {
     x: "square_cross",
@@ -10,11 +12,19 @@ const classes = {
     active: "square_active"
 };
 
+const statuses = {
+    default: "Tic Tac Toe",
+    win: "You win",
+    lose: "You lose",
+    tie: "Tie"
+};
+
 squares.forEach((el) => {
     const number = el.getAttribute("data-number");
 
     el.addEventListener("click", function () {
         if (game_is_active && field[number] === undefined) {
+            turn++;
             this.classList.add(classes[sign]);
             setTimeout(() => this.classList.add(classes["active"]));
             field[number] = sign;
@@ -32,11 +42,17 @@ function change_sign() {
 
 function reset() {
     sign = "x";
+    set_status("default");
+    turn = 0;
     field = Array(9);
     squares.forEach((el) => {
         el.classList.remove(classes["x"], classes["y"], classes["active"])
-    })
+    });
     game_is_active = true;
+}
+
+function set_status(status) {
+    status_container.innerText = statuses[status];
 }
 
 function check_win() {
@@ -48,7 +64,14 @@ function check_win() {
         field[2] === sign && field[5] === sign && field[8] === sign ||
         field[0] === sign && field[4] === sign && field[8] === sign ||
         field[2] === sign && field[4] === sign && field[6] === sign) {
-        console.log("win " + sign);
+        if(sign === "x") {
+            set_status("win")
+        } else {
+            set_status("lose")
+        }
+        game_is_active = false;
+    } else if(turn === 9) {
+        set_status("tie");
         game_is_active = false;
     }
 }
