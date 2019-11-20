460a8ace6cc61c485eb4b7cf388901d9fccb49bc
Daniil <daniel217032001@gmail.com>
2019-11-20 19:03:34 +0300

    add function check_win

--- src/scripts/app/game.js
+++ src/scripts/app/game.js
@@ -1,6 +1,7 @@
 let field = Array(9);
 const squares = document.querySelectorAll(".js-square");
 const reset_btn = document.querySelector(".js-reset");
+let game_is_active = true;
 let sign = "x";
 
 const classes = {
@@ -12,13 +13,14 @@ const classes = {
 squares.forEach((el) => {
     const number = el.getAttribute("data-number");
 
-        el.addEventListener("click", function () {
-            if(field[number] === undefined) {
-                this.classList.add(classes[sign], classes["active"]);
-                field[number] = sign;
-                change_sign();
-            }
-        })
+    el.addEventListener("click", function () {
+        if (game_is_active && field[number] === undefined) {
+            this.classList.add(classes[sign], classes["active"]);
+            field[number] = sign;
+            check_win();
+            change_sign();
+        }
+    })
 });
 
 reset_btn.addEventListener("click", reset);
@@ -33,4 +35,19 @@ function reset() {
     squares.forEach((el) => {
         el.classList.remove(classes["x"], classes["y"], classes["active"])
     })
+    game_is_active = true;
+}
+
+function check_win() {
+    if (field[0] === sign && field[1] === sign && field[2] === sign ||
+        field[3] === sign && field[4] === sign && field[5] === sign ||
+        field[6] === sign && field[7] === sign && field[8] === sign ||
+        field[0] === sign && field[3] === sign && field[6] === sign ||
+        field[1] === sign && field[4] === sign && field[7] === sign ||
+        field[2] === sign && field[5] === sign && field[8] === sign ||
+        field[0] === sign && field[4] === sign && field[8] === sign ||
+        field[2] === sign && field[4] === sign && field[6] === sign) {
+        console.log("win " + sign);
+        game_is_active = false;
+    }
 }
